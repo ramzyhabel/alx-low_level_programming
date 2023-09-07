91c6e53249baa837b9efb4a18009f2b0c6ae7168
ramzyhabel <ramzyi081@gmail.com>
2023-09-07 18:11:48 +0000

    Done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - enty point
+ *
+ * Description: C program that prints using a printf function.
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
