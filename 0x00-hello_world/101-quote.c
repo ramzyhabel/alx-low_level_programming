209a8471851cbd6549e223515a8957426f22b725
ramzyhabel <ramzyi081@gmail.com>
2023-09-07 19:05:47 +0000

    Done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+/**
+ * main - enty point
+ *
+ * Description: C program that prints a quote using the write function
+ *
+ * Return: Always 1 (unsuccess)
+*/
+
+int main(void)
+{
+	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quote, 59);
+	return (1);
+}
