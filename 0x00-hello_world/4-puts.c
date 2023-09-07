a57cfc210104478fa193ffe4a6f5a53ed3a4af64
ramzyhabel <ramzyi081@gmail.com>
2023-09-07 17:56:47 +0000

    Done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+
+/**
+ * main - Enty point
+ *
+ * Description: C program that uses puts function for printing
+ *
+ * Return: Always 0 (success)
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
-	return(0);
+	return (0);
 };
