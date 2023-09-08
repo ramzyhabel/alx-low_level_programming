514b110a6f447ac23d61b9acbb2299fe6f776fe9
ramzyhabel <ramzyi081@gmail.com>
2023-09-08 17:15:41 +0000

    Done

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - enty point
+ *
+ * Description: C Program that prints 1:9 using putchar
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	char n = 49;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
