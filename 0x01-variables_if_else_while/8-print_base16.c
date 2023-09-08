3c130f80a313babc2262ade7afd4f7cbac47097d
ramzyhabel <ramzyi081@gmail.com>
2023-09-08 17:35:03 +0000

    Done

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - enty point
+ *
+ * Description: C program that prints hex numbers
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	char first_number = 48;
+
+	while (first_number >= 102)
+	{
+		if (first_number == 58)
+		{
+			first_number = 97;
+		}
+		putchar(first_number);
+		first_number++;
+	}
+	putchar('\n');
+	return (0);
+}
