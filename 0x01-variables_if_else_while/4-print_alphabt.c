9ac0bc96bdb8f200212232b4b31e94531685a68f
ramzyhabel <ramzyi081@gmail.com>
2023-09-08 16:53:02 +0000

    Done

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	char first_letter = 97;
 
-	while (first_letter <= 122 && first_letter != 101 && first_letter != 113)
+	while (first_letter <= 122 || first_letter != 101 || first_letter != 113)
 	{
 		putchar(first_letter);
 		first_letter++;
