eb1a4f01cd734ce486b6f733baf7643d515a3907
ramzyhabel <ramzyi081@gmail.com>
2023-09-08 16:49:10 +0000

    Done

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - enty point
+ *
+ * Description: C program print alphabet expect o & q
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	char first_letter = 97;
+
+	while (first_letter <= 122 && first_letter != 101 && first_letter != 113)
+	{
+		putchar(first_letter);
+		first_letter++;
+	}
+	putchar('\n');
+}
