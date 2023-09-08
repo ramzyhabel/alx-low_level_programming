c13fa5a0a585e258d67464546261ee5f50be08e7
ramzyhabel <ramzyi081@gmail.com>
2023-09-08 16:15:14 +0000

    Done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - enty point
+ *
+ * Description: C program to print the alphabet using putchar
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	char first_letter = 97;
+
+	while (first_letter >= 97 || first_letter <= 122)
+	{
+	putchar(first_letter);
+	first_letter++;
+	}
+	return (0);
+}
