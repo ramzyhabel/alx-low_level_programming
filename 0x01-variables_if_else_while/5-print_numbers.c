68e571cedda8882c9f550a0bbfecb7afd1e89804
ramzyhabel <ramzyi081@gmail.com>
2023-09-08 17:07:19 +0000

    Done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - enty point
+ *
+ * Description: C program that prints numbers 0:9
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	int n = 0;
+
+	while (n <= 9)
+	{
+		printf("%i\n", n);
+	}
+	return (0);
+}
