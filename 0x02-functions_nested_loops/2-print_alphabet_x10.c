16552cffc761fe1abe5cbb14e878e39eab8643d0
ramzyhabel <ramzyi081@gmail.com>
2023-09-12 14:09:26 +0000

    Done

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include <main.h>
+/**
+ * print_alphabet_x10 - enty point
+ *
+ * Description: prints 10 times the alphabet, in lowercase
+ *
+ * Return: Always 0 (success)
+*/
+void print_alphabet_x10(void)
+{
+	int ch;
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+	return (0);
+}
