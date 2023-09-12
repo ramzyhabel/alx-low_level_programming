5f730ca8ddd6620334c86b8c4be92fc36d45ea50
ramzyhabel <ramzyi081@gmail.com>
2023-09-12 13:44:17 +0000

    Done

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - enty point
+ *
+ * Description: C program prints alphabet with print_alphabet function
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/print_alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - entry point
+ *
+ * Description: prints the alphabet, in lowercase, followed by a new line
+ *
+ * Return: void
+ *
+ */
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a', ch <= 'z', ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
