290120c989563d73f4bf32d2a1435ff1a6ec1886
ramzyhabel <ramzyi081@gmail.com>
2023-09-12 16:41:24 +0000

    Done

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * add - enty point
+ *
+ * Description: add two numbers
+ *
+ * @a: number to be added
+ *
+ * @b: number to be added
+ *
+ * Return: addition result
+*/
+int add(int a, int b)
+{
+	int r;
+
+	r = a + b;
+
+	return (r);
+}
