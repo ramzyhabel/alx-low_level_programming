c95f020acfcfbe2230b53ef88e5c1794c2056322
ramzyhabel <ramzyi081@gmail.com>
2023-09-12 14:49:48 +0000

    Done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - enty point
+ *
+ * Description: computes the absolute value of an integer
+ *
+ * @int: i/p
+ *
+ * Return: 0 (sucess)
+*/
+int _abs(int)
+{
+	if (n < 0)
+	{
+		n = (-1) * n;
+	}
+	return (n);
+}
