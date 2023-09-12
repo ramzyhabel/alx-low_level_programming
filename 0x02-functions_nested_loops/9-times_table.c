0cea3ce274dd988782b14b40c94a4f66c02f33d9
ramzyhabel <ramzyi081@gmail.com>
2023-09-12 16:13:50 +0000

    Done

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * times_table - enty point
+ *
+ * Description:  prints the 9 times table, starting with 0
+ *
+ * Return: void
+*/
+void times_table(void)
+{
+	int n = 9;
+	int i;
+	int r;
+
+	for (i = 0; i <= 10; i++)
+	{
+		r = i * n
+		_putchar(r + 48);
+		_putchar('\n');
+	}
+}
