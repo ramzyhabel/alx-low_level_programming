5ac793f58d8105e279dac7da6bf2db98f61b1fa8
ramzyhabel <ramzyi081@gmail.com>
2023-09-14 17:26:40 +0000

    Done

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_line - enty point
+ *
+ * Description: draws a straight line in the terminal
+ *
+ * @n: line length
+ *
+ * Return: Always 0 (success)
+*/
+void print_line(int n)
+{
+	int l;
+
+	if (n > 0)
+	{
+		for (l = n; l <= n; l++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
