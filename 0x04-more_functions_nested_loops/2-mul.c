85073a74c710f300ce39c341f26ad0265b4ccc57
ramzyhabel <ramzyi081@gmail.com>
2023-09-14 15:53:11 +0000

    Done

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * mul - enty point
+ *
+ * Description: add two mul
+ *
+ * @a: number to be mul
+ *
+ * @b: number to be mul
+ *
+ * Return: multiplication result
+*/
+int mul(int a, int b)
+{
+	int r;
+
+	r = a * b;
+
+	return (r);
+}
