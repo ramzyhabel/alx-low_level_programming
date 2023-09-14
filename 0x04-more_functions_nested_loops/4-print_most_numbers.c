b393a9c868c58cd6f3ce89e4f0c49ff5e6260b83
ramzyhabel <ramzyi081@gmail.com>
2023-09-14 16:09:19 +0000

    Done

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_most_numbers - enty point
+ *
+ * Description: prints numbers expect 2 & 4
+ *
+ * Return: Always 0 (success)
+*/
+void print_most_numbers(void)
+{
+	int ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		if (ch == '2' || ch == '4')
+		{
+			ch == '127';
+		}
+		else
+		{
+		_putchar(ch);
+		}
+	}
+	_putchar('\n');
+}
