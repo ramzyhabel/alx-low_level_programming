9167b05fb9c70ebaba27fa11653dc5fe92e4c00e
ramzyhabel <ramzyi081@gmail.com>
2023-09-14 18:12:52 +0000

    Done

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -18,9 +18,9 @@ void print_diagonal(int n)
 	}
 	else
 	{
-		for (p = 1; p <= n; p++)
+		for (p = 0; p <= n; p++)
 		{
-			for (s = 1; s <= p; s++)
+			for (s = 0; s <= p; s++)
 			{
 				_putchar(' ');
 			}
