e020cfb8a29b7a59b496b0ceea1a7f1815c9fe81
ramzyhabel <ramzyi081@gmail.com>
2023-09-14 13:12:40 +0000

    Done

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,31 +1,6 @@
 #include "main.h"
 /**
- * positive_or_negative - chech number +ve or -ve
- * Description: C program to print if a variable is +ve or -ve
- * @n: number to be checked
- * Return: Always 0 (success)
-*/
-int positive_or_negative(int n)
-{
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	{
-		printf("%i is positive\n", n);
-	}
-	else if (n < 0)
-	{
-		printf("%i is negative\n", n);
-	}
-	else
-	{
-		printf("%i is zero\n", n);
-	}
-	return (0);
-}
-/**
- * main - main function
+ * main - chech number +ve or -ve
  * Description: C program to print if a variable is +ve or -ve
  * Return: Always 0 (success)
 */
@@ -33,7 +8,7 @@ int main(void)
 {
 	int i;
 
-	i = 0;
+	i = 98 % 2;
 
 	positive_or_negative(i);
 
